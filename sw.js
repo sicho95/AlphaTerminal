3e7e1abd16f93ab2388d786a5cb29a27242f5e12
sicho95 <sicho95@users.noreply.github.com>
2026-06-09 16:43:39 +0200

    Improve desktop layout density

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'alphaterminal-v16';
+const CACHE_VERSION = 'alphaterminal-v17';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const DATA_CACHE = `${CACHE_VERSION}-data`;
 
